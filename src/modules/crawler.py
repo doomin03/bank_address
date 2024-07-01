0490dd7b47710bab1942c4a7c9c76c187afa9fe8
doomin03 <111326974+doomin03@users.noreply.github.com>
2024-07-02 00:08:51 +0900

    크롤링 기능 완성

--- src/modules/crawler.py
+++ src/modules/crawler.py
@@ -8,6 +8,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
 import time
+import urllib.parse
 
 
 
@@ -27,7 +28,7 @@ class address_crawler():
         chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36")
 
         # Chrome 프로세스 최적화
-        chrome_options.add_argument("single-process")  # 단일 프로세스 모드
+        # chrome_options.add_argument("single-process")  # 단일 프로세스 모드
         chrome_options.add_argument("--disable-dev-shm-usage")  # 공유 메모리 사용 비활성화
         chrome_options.add_argument("--disable-dev-tools")  # 개발자 도구 비활성화
         chrome_options.add_argument("--no-zygote")  # Zygote 비활성
@@ -39,24 +40,62 @@ class address_crawler():
         chrome_options.add_experimental_option("detach", True)
 
         self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-        self.wait = WebDriverWait(self.driver, 15)
+        self.wait = WebDriverWait(self.driver, 20)
 
     
 
 
     def get_address(self, search)->None:
-        url:str = "https://map.naver.com/p/search/"+search+"?c=5.00,0,0,0,dh"
+        encoded_search = urllib.parse.quote(search)  # 검색어 인코딩
+        url: str = f"https://map.naver.com/p/search/{encoded_search}?c=13.00,0,0,0,dh"
         self.driver.get(url=url)
+
+
         try:
-            
-            main_div = self.wait.until(
-                EC.visibility_of_element_located((By.XPATH, "//*[@id='app-root'] //*[@id='_pcmap_list_scroll_container]/ul']"))
+            self.wait.until(EC.frame_to_be_available_and_switch_to_it((By.CSS_SELECTOR, "#searchIframe")))
+
+            search_result_div = self.wait.until(
+                EC.visibility_of_element_located((By.XPATH, "//*[@id='_pcmap_list_scroll_container']"))
             )
 
-            address = main_div.find_elements(By.CSS_SELECTOR, "#_pcmap_list_scroll_container > ul > li:nth-child(1) > div.qbGlu > div.ouxiq > div > div > span:nth-child(2) > a > span.Pb4bU")        
+            last_height = self.driver.execute_script("return arguments[0].scrollHeight", search_result_div)
+            while True:
+                # 스크롤을 아래로
+                self.driver.execute_script("arguments[0].scrollTo(0, arguments[0].scrollHeight);", search_result_div)
+                time.sleep(2)  # 스크롤 후 로딩 대기
+
+                # 스크롤 후 높이 계산
+                new_height = self.driver.execute_script("return arguments[0].scrollHeight", search_result_div)
+                if new_height == last_height:  # 더 이상 스크롤할 내용이 없으면 중단
+                    break
+                last_height = new_height
+
+            search_result = self.wait.until(
+                EC.visibility_of_element_located((By.XPATH, "//*[@id='_pcmap_list_scroll_container']/ul"))
+            )
+
+
+            search_results = search_result.find_elements(By.CSS_SELECTOR, "li.VLTHu.OW9LQ")
+            
+            for result in search_results:
+                try:
+                    name = result.find_element(By.CSS_SELECTOR, "span.YwYLL").text  # 장소 이름
+                    print(f"Name: {name}")
+                except Exception as e:
+                    print(f"결과 처리 중 오류 발생: {str(e)}")
+
+                try:
+                    button = result.find_element(By.CSS_SELECTOR, " div.KgfA6.D7FgR> span.lWwyx > a")
+                    button.click()
+                    address = result.find_element(By.CSS_SELECTOR, "div.KgfA6.D7FgR div > div.AbTyi > div.zZfO1").text
+                    print(f"address: {address}")
+                except Exception as e:
+                    print(f"결과 처리 중 오류 발생: {str(e)}")
         except Exception as e:
-            print("실패")
+            print(f"실패: {str(e)}")
 
     
 
 
+ad = address_crawler()
+ad.get_address("농협은행 지점")
